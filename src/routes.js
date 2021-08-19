a663d1dd905789a09756504bdf922fa5ef5384f8
montanari2019 <ikaro.montanari31@gmail.com>
2021-08-19 15:33:07 -0400

    Tentando resolver conflitos do git

--- src/routes.js
+++ src/routes.js
@@ -12,11 +12,4 @@ routes.get('/associacaoIndex',AssociacaoController.index)
 routes.put('/associacaoUpdate/:id',AssociacaoController.update)
 routes.delete('/associacaoDelete/:id',AssociacaoController.delete)
 
-// Model de dados bancarios de cada associação
-// routes.post('/associacao/dadosbancariosStore', DadosBancariosController.store);
-// routes.get('/associacao/dadosbancarios', DadosBancariosController.index);
-// routes.get('/associacao/dadosbancarios/:id', DadosBancariosController.indexSelect);
-// routes.put('/associacao/dadosbancariosUpdate/:id', DadosBancariosController.update);
-// routes.delete('/associacao/dadosbancariosDelete/:id', DadosBancariosController.delete);
-
 module.exports = routes;
