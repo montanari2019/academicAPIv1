7440529c1b1f6682b05c7518944e3985bd5a629d
montanari2019 <ikaro.montanari31@gmail.com>
2021-08-19 16:51:13 -0400

    Configurando para deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'admin',
-    database: 'academic',
+    dialect: process.env.DIALECT,
+    host: process.env.HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     define: {
         timestamps: true,
         underscored: true,
